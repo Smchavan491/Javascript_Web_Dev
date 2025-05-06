46bbc656fe73c1638debae5274c44524764e18eb
Samrudhi <samrudhi.chavan.2004@gmail.com>
2025-05-06 18:40:13 +0530

    Simple Static Website

---
+++ Node.js_simple_static website/public/script.js
@@ -0,0 +1,13 @@
+function sayHello() {
+    alert('Hello from static website!');
+  }
+
+function showAlert() {
+    alert('This is an alert message!');
+  }
+
+function red(){
+    console.log('Red button clicked!');
+    document.body.style.backgroundColor = 'pink';
+
+}
