d80b8fa3ff010f526661c4b8478bab5f26036947
Samrudhi <samrudhi.chavan.2004@gmail.com>
2025-05-07 20:04:23 +0530

    Initial Commit

---
+++ Angular.js/angular/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(private router: Router) {}
+    onLogin(){
+      console.log('Login clicked');
+      this.router.navigate(['/home']);
+    }
+}
---
+++ Angular.js/angular/src/app/registration/registration.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registration',
+  standalone: false,
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.css'
+})
+export class RegistrationComponent {
+
+  constructor(private router: Router) {}
+    onRegister(){
+      console.log('User register successfully');
+      this.router.navigate(['login']);
+    }
+
+}
